ffbefe7018c5c3d88814604e46048ff126506b92
monikatyagi01 <143579790+monikatyagi01@users.noreply.github.com>
2025-02-14 09:39:49 +0530

    leetcode 287

---
+++ find the duplicate.cpp
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/find-the-duplicate-number/
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+         unordered_map<int,int> numsco;  
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+           if( numsco.find(nums[i])!=numsco.end())
+          {
+             return nums[i];
+          }
+             numsco[nums[i]]++;
+        }
+        return -1;
+
+        
+    }
+};
