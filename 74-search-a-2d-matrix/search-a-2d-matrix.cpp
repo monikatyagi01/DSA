a790c536110279f40c7f3626788cd056167c9b51
monikatyagi01 <143579790+monikatyagi01@users.noreply.github.com>
2025-02-19 12:34:57 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (7.78%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -2,13 +2,10 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-    
         for(auto i:matrix)
         {
-            if(find(i.begin(),i.end(),target)!=i.end())
-            {
+            if(find(i.begin(),i.end(),target)!=i.end()) 
             return true;
-        }
         }
         return false;
     }
