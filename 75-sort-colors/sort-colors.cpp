bbf49de0b553a2a8dc1fed8ffb5d84b067161ae3
monikatyagi01 <143579790+monikatyagi01@users.noreply.github.com>
2025-08-05 21:36:15 +0530

    Time: 6 ms (0.84%) | Memory: 11.7 MB (48.66%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int l=nums.size();
+        for(int i=0;i<l;i++)
+        {
+            for(int j=i;j<l;j++){
+                if(nums[j]<nums[i])
+                {
+                    int x=nums[j];
+                    nums[j]=nums[i];
+                    nums[i]=x;
+                }
+            }
+        }
+        for(auto num: nums)
+        {
+            cout<<num;
+        }
+    }
+};
