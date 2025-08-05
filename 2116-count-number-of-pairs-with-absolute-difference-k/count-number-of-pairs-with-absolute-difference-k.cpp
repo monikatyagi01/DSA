e199e449fd987dbef31f9c41fd55be5da8fdf556
monikatyagi01 <143579790+monikatyagi01@users.noreply.github.com>
2025-08-05 20:26:58 +0530

    Time: 4 ms (40.02%) | Memory: 16 MB (57.07%) - LeetSync

---
+++ 2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countKDifference(vector<int>& nums, int k) {
+        
+        unordered_map<int,int> umap;
+        int count=0;
+        int l=nums.size();
+      /*  for(auto num:nums)
+        {
+            umap[num]++;
+            l++;
+        }*/
+        for(int i=0;i<l;i++)
+        {
+              for(int j=0;j<l;j++){
+          //  int x=abs(num1-k);
+          // if(umap.find(x)!=umap.end()){
+            if(abs(nums[i]-nums[j])==k)
+           count++;
+          // umap[num1]--;
+           }
+        }
+        
+        return count/2;
+    }
+};
